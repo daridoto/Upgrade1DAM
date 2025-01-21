8cf6264c2f6008a1f793b85dcf8bb96b1f3760aa
daridoto <rudasa7@gmail.com>
2025-01-21 10:22:29 +0100

    Update Funciones.java

--- Actividades/Funciones.java
+++ Actividades/Funciones.java
@@ -1,5 +1,14 @@
 public class Funciones {
-  public static void main(String[] args){
-    
-  }
+  //Procedo con la creacion de la funcion calcular para la media:
+	public static int calcular(int[] numerosEnteros) {
+		int suma = 0; 
+		for (int numero : numerosEnteros) {
+			suma += numero;
+		}
+		return suma / numerosEnteros.length;
+	}
+	public static void main(String[] args) {
+		int[] numerosEnteros = {1, 2, 3}; 
+		System.out.println(calcular(numerosEnteros));
+	}	
 }
